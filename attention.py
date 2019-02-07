74b574f30cd75df20763c08c79a2dc5d2f162810
Pranay <mirage_1997@mail.ru>
2019-02-07 16:33:53 +0530

    attention fixed

--- attention.py
+++ attention.py
@@ -8,15 +8,23 @@ class Attention(nn.Module):
 	This class implements soft-attention
 	'''
 
-	def __init__(self, attention_size: int):
+	def __init__(self, attention_size: int, batch_first: bool = True):
 
 		self.attention_size: int = attention_size
 		self.attention_tensor: torch.Tensor = torch.empty(attention_size, 1)
 		torch.nn.init.xavier_normal_(self.attention_tensor.weight)
 
-	def forward(self, attention_candidates: torch.Tensor, weighted_sum_candidates: torch.Tensor = None) -> torch.Tensor:
+	def forward(self, attention_candidates: torch.Tensor, weighted_sum_candidates: torch.Tensor = None, batch_first: bool = True) -> torch.Tensor:
 
-		weights = F.softmax(torch.matmul(attention_candidates, self.attention_tensor), dim=1)
+		
+		if batch_first:
+			weights = F.softmax(torch.matmul(attention_candidates, self.attention_tensor), dim=1)
+		else:
+			dummy = torch.Tensor(attention_candidates.size(1), attention_candidates.size(0), attention_candidates.size(2))
+			for i in range(attention_candidates.size(1)):
+				dummy[i] = torch.cat([attention_candidates[j, i, :] for j in range(attention_candidates.size(0))],
+					 					dim=0).view(attention_candidates.size(0), attention_candidates.size(2))
+
 		
 		if weighted_sum_candidates is None:
 			weighting = weights * attention_candidates
