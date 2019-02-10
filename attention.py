2e2ff8589694bc474d8c2b1b3a4d6f7df35e2074
53X <mirage_1997@mail.ru>
2019-02-11 01:34:56 +0530

    changes to attention

--- attention.py
+++ attention.py
@@ -2,43 +2,38 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F 
 
-class Attention(nn.Module):
+class Attention(flair.nn.Model):
 
-	'''
-	This class implements soft-attention
-	'''
+    '''
+    This class implements soft-attention
+    '''
 
-	def __init__(self, attention_size: int, batch_first: bool = True):
+    def __init__(self, batch_first: bool = True):
 
-		super(Attention, self).__init__()
-		self.attention_size: int = attention_size
-		
-		
+        super(Attention, self).__init__()
+        self.batch_first: bool = batch_first
+        
 
-	def forward(self, attention_candidates: torch.Tensor, weighted_sum_candidates: torch.Tensor = None, batch_first: bool = True) -> torch.Tensor:
+	def forward(self, , attention_size: int, attention_candidates: torch.Tensor, weighted_sum_candidates: torch.Tensor = None) -> torch.Tensor:
 
-		self.attention_tensor: torch.Tensor = torch.empty(attention_candidates.size(0), attention_size, 1) 
-		torch.nn.init.ones_(self.attention_tensor)
+        self.attention_tensor: torch.Tensor = torch.empty(attention_candidates.size(0), attention_size, 1) 
+        torch.nn.init.ones_(self.attention_tensor)
 
-		if batch_first:
-			weights = F.softmax(torch.matmul(attention_candidates, self.attention_tensor), dim=1)
-		else:
-			dummy = torch.empty(attention_candidates.size(1), attention_candidates.size(0), attention_candidates.size(2))
-			
-			for i in range(attention_candidates.size(1)):
-				dummy[i] = torch.cat([attention_candidates[j, i, :] for j in range(attention_candidates.size(0))],
-					 				 dim=0).view(attention_candidates.size(0), attention_candidates.size(2))
+        if self.batch_first:
+            weights = F.softmax(torch.matmul(attention_candidates, self.attention_tensor), dim=1)
+        else:
+            dummy = torch.empty(attention_candidates.size(1), attention_candidates.size(0), attention_candidates.size(2))
+            
+            for i in range(attention_candidates.size(1)):
+                dummy[i] = torch.cat([attention_candidates[j, i, :] for j in range(attention_candidates.size(0))],
+                                     dim=0).view(attention_candidates.size(0), attention_candidates.size(2))
 
-			weights = F.softmax(torch.matmul(dummy, self.attention_tensor), dim=1)	
+            weights = F.softmax(torch.matmul(dummy, self.attention_tensor), dim=1)  
 
-		
-		if weighted_sum_candidates is None:
-			weighting = weights * attention_candidates
-		else:
-			weighting = weights * weighted_sum_candidates	
+        
+        if weighted_sum_candidates is None:
+            weighting = torch.matmul(weights, attention_candidates)
+        else:
+            weighting = torch.matmul(weights, weighted_sum_candidates)  
 
-		return torch.sum(weighting, dim=1, keepdim=True)
-
-
-
-
+        return weighting
